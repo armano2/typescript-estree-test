4471a82f7de5dd78b227c0bec104a202fbe3eb63
Armano <armano2@users.noreply.github.com>
2018-12-22 23:44:37 +0100

    add report

--- run-tests.ts
+++ run-tests.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import glob = require('tiny-glob');
 import { parse } from 'typescript-estree/dist/parser';
 
-const errors = new Map<string, string>();
+const errors = new Map<string, string[]>();
 
 glob('projects/**/*.ts', {
   cwd: __dirname,
@@ -19,11 +19,28 @@ glob('projects/**/*.ts', {
           errorOnUnknownASTType: true
         });
       } catch (e) {
-        errors.set(e.message, path.relative(__dirname, file));
+        const filePath = path.normalize(path.relative(__dirname, file));
+        const error = errors.get(e.message);
+        if (error) {
+          error.push(filePath);
+        } else {
+          errors.set(e.message, [filePath]);
+        }
       }
     }
 
-    console.error(Array.from(errors));
+    const toReport = {
+      messages: Array.from(errors).map(item => item[0]),
+      details: Array.from(errors).map(item => {
+        return {
+          message: item[0],
+          files: item[1]
+        };
+      })
+    };
+
+    fs.writeFileSync('./report.json', JSON.stringify(toReport, null, 2));
+    console.log('report saved.');
   })
   .catch(e => {
     throw e;
