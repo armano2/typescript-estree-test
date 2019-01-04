2c612ac597ea51f0ae15b4f0d4c114c796434757
Armano <armano2@users.noreply.github.com>
2019-01-04 22:28:01 +0100

    correct alignment tests

--- src/alignment.spec.ts
+++ src/alignment.spec.ts
@@ -7,30 +7,28 @@ import assert from 'assert';
 describe('alignment', () => {
   const files = readFixtures();
 
-  context('tests', function() {
-    for (const file of files) {
-      const filePath = path
-        .normalize(path.relative(__dirname, file))
-        .replace(/\\/g, '/');
+  for (const file of files) {
+    const filePath = path
+      .normalize(path.relative(__dirname, file))
+      .replace(/\\/g, '/');
 
-      it(`${filePath}`, function() {
-        return readFixture(file).then(({ content, isTsx }) => {
-          const bCode = preprocessBabelAST(parseBabel(content, isTsx));
-          if (bCode.parseError) {
-            return;
-          }
+    it(`${filePath}`, function() {
+      return readFixture(file).then(({ content, isTsx }) => {
+        const bCode = preprocessBabelAST(parseBabel(content, isTsx));
+        if (!bCode || bCode.parseError) {
+          return;
+        }
 
-          const tsCode = parseTsEstree(content, isTsx);
-          if (tsCode.parseError) {
-            return;
-          }
+        const tsCode = parseTsEstree(content, isTsx);
+        if (!tsCode || tsCode.parseError) {
+          return;
+        }
 
-          assert.deepStrictEqual(
-            removeFromProgramNode(tsCode),
-            removeFromProgramNode(bCode)
-          );
-        });
+        assert.deepStrictEqual(
+          removeFromProgramNode(tsCode),
+          removeFromProgramNode(bCode)
+        );
       });
-    }
-  });
+    });
+  }
 });
